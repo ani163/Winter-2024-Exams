35bc2d895f4d685c9d7f909afab5c0b3340a27df
ani163 <melnika243@gmail.com>
2024-01-13 19:53:08 +0200

    Use arrow functions for better readability

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -2,7 +2,7 @@
 
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zipArrays = function (arr1 = [], arr2 = []) {
+const zipArrays = (arr1 = [], arr2 = []) => {
   const result = [];
   let i = 0;
 
