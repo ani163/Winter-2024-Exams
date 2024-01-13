88cd019631db58264a28d515e25ca6c0e1fdadd4
ani163 <melnika243@gmail.com>
2024-01-14 01:19:11 +0200

    Changed iteration implementation in zip.js

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -5,14 +5,9 @@
 
 const zipArrays = (arr1 = [], arr2 = []) => {
   const result = [];
-  let i = 0;
 
-  for (const elem of arr2) {
-    const cell = [arr1[i++], elem];
-
-    if (cell[0] !== undefined) {
-      result.push(cell);
-    }
+  for (let i = 0; i < Math.min(arr1.length, arr2.length); i++) {
+    result.push([arr1[i], arr2[i]]);
   }
 
   return result;
