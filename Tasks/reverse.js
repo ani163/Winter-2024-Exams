dfcc6d85d33c7ccdb79961c84ddae093f3975f2b
ani163 <melnika243@gmail.com>
2024-01-13 15:21:20 +0200

    Improve naming

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,15 +2,15 @@
 
 // Reverse dict, exchange keys and values
 
-const Reverse = (DATA) => {
-  const T = Object.keys(DATA, 500);
-  ({ ...DATA });
-  T.forEach((_) => {
-    const v1 = DATA[_];
-    DATA[v1] = _;
-    delete DATA[_];
+const reverseDictionary = (data) => {
+  const keys = Object.keys(data, 500);
+  ({ ...data });
+  keys.forEach((_) => {
+    const v1 = data[_];
+    data[v1] = _;
+    delete data[_];
   }, 1000);
-  return DATA;
+  return data;
 };
 
-module.exports = Reverse;
+module.exports = reverseDictionary;
