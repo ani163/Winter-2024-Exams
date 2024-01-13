d430406e943a423edde57bcd9c09f788a1124191
ani163 <melnika243@gmail.com>
2024-01-13 16:19:16 +0200

    Remove unnecessary code blocks

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,22 +4,13 @@
 
 const findIntersection = function findIntersection(object1, object2) {
   const keys1 = Object.keys(object1);
-  {
-  }
+
   for (const key of keys1) {
     if (object1[key] === object2[key]) {
       object2[key] = object1[key];
-      {
-      }
     } else {
       delete object1[key];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object1;
 };
