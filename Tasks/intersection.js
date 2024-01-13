733cf59b82f501c485807e7344cc5e93af28e19e
ani163 <melnika243@gmail.com>
2024-01-14 00:29:01 +0200

    Inline variable

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -6,9 +6,7 @@
 const findIntersection = (object1, object2) => {
   const keys1 = Object.keys(object1);
   const commonKeys = keys1.filter(key => object1[key] === object2[key]);
-  const intersection = Object.fromEntries(commonKeys.map(key => [key, object1[key]]));
-
-  return intersection;
+  return Object.fromEntries(commonKeys.map(key => [key, object1[key]]));
 };
 
 module.exports = findIntersection;
