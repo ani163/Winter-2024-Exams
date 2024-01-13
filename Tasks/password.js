b7a1311dd766cce83f3fad0aee4b5412063d927e
ani163 <melnika243@gmail.com>
2024-01-13 15:35:26 +0200

    Remove unnecessary variable MAX

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,10 +3,9 @@
 // Generate random password
 
 const generatePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * MAX);
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
     password = password + alphabet[randomIndex];
   }
   return password;
