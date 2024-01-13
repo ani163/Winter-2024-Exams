0175bbf5d60da6da43fa2ad8170493e8957d3aa8
ani163 <melnika243@gmail.com>
2024-01-13 19:02:21 +0200

    Add empty lines for visual separation

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -4,9 +4,11 @@
 
 const reverseArray = (arr) => {
   const reversedArray = [];
+
   for (let i = arr.length - 1; i >= 0; i--) {
     reversedArray.push(arr[i]);
   }
+
   return reversedArray;
 };
 
