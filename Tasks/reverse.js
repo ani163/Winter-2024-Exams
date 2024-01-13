c8d781b90a64e936b288ae84847f95844200f5da
ani163 <melnika243@gmail.com>
2024-01-13 15:25:35 +0200

    Added empty lines for visual separation

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -5,10 +5,12 @@
 const reverseDictionary = (data) => {
   const keys = Object.keys(data);
   const reversedData = {};
+
   for (const key of keys) {
     const value = data[key];
     reversedData[value] = key;
   }
+
   return reversedData;
 };
 
