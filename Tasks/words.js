abdfcd45847cfd28510768142d9e6bccec232042
ani163 <melnika243@gmail.com>
2024-01-13 17:34:57 +0200

    Remove unnecessary flags and simplify the logic for counting words

--- Tasks/words.js
+++ Tasks/words.js
@@ -5,37 +5,9 @@
 const countWords = (inputString) => {
   const wordsArray = inputString.trim().split(/\s+/);
   let numberOfWords = 0;
-  flag = false;
-  for (c of inputString) {
-    if (!flag) {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-        numberOfWords++;
-      }
-    } else {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-      }
+  for (const word of wordsArray) {
+    if (word !== '') {
+      numberOfWords++;
     }
   }
   return numberOfWords;
