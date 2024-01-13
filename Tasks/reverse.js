c3d46786510ea513210d4b7e0614def00f3ea12f
ani163 <melnika243@gmail.com>
2024-01-13 15:22:07 +0200

    Removed unnecessary operations and parameters

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -3,13 +3,12 @@
 // Reverse dict, exchange keys and values
 
 const reverseDictionary = (data) => {
-  const keys = Object.keys(data, 500);
-  ({ ...data });
+  const keys = Object.keys(data);
   keys.forEach((_) => {
     const v1 = data[_];
     data[v1] = _;
     delete data[_];
-  }, 1000);
+  });
   return data;
 };
 
