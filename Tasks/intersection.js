7647d100e2c517ebbd231068616cc5109cefc538
ani163 <melnika243@gmail.com>
2024-01-14 00:27:31 +0200

    intersection.js without delete

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -5,13 +5,8 @@
 
 const findIntersection = (object1, object2) => {
   const keys1 = Object.keys(object1);
-  const intersection = object1;
-
-  for (const key of keys1) {
-    if (object1[key] !== object2[key]) {
-      delete intersection[key];
-    }
-  }
+  const commonKeys = keys1.filter(key => object1[key] === object2[key]);
+  const intersection = Object.fromEntries(commonKeys.map(key => [key, object1[key]]));
 
   return intersection;
 };
