3d6622bc6dd69a00cbf789646480548cbd51025e
ani163 <melnika243@gmail.com>
2024-01-13 20:23:14 +0200

    Remove variable len

--- Tasks/split.js
+++ Tasks/split.js
@@ -4,7 +4,6 @@
 
 const splitArray = (index, array) => {
   const beginning = array.slice(0, index);
-  const len = array.length;
   array = array.slice(index, len);
   return [beginning, array];
 };
