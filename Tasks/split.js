adfeaf611133e48534e4feb8df9723478ef57fee
ani163 <melnika243@gmail.com>
2024-01-13 20:21:14 +0200

    Add const where needed

--- Tasks/split.js
+++ Tasks/split.js
@@ -2,8 +2,8 @@
 
 // Split an array into two parts
 
-let split_Array = (index, array) => {
-  begin = array.slice(0, index);
+const split_Array = (index, array) => {
+  const begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
