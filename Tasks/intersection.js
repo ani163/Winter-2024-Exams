61dad5fc4850b4e85ae33511190c785dfb16fecb
ani163 <melnika243@gmail.com>
2024-01-13 16:23:22 +0200

    Simplify the condition inside the loop

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -6,9 +6,7 @@ const findIntersection = (object1, object2) => {
   const keys1 = Object.keys(object1);
 
   for (const key of keys1) {
-    if (object1[key] === object2[key]) {
-      object2[key] = object1[key];
-    } else {
+    if (object1[key] !== object2[key]) {
       delete object1[key];
     }
   }
